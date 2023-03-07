ec2398feae1cde329934ae7141a00c241f0121b3
Tianqi Tang <tianqit@usc.edu>
2023-03-07 04:25:42 -0800

    flake8

--- training/scripts/fasta_to_onehot.py
+++ training/scripts/fasta_to_onehot.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('..')
-
 import numpy as np
 import ContigNet.util as util
 import os
@@ -8,36 +5,39 @@ from Bio import SeqIO
 
 import argparse
 
+
 def fasta_to_onehot_dir(input_dir, output_dir):
 
-    fasta_ext = ('.fna', '.fasta', '.fa')
+    fasta_ext = (".fna", ".fasta", ".fa")
 
     input_fasta = os.listdir(input_dir)
     input_fasta = filter(lambda x: x.endswith(fasta_ext), input_fasta)
 
     for fasta in input_fasta:
         fasta_path = os.path.join(input_dir, fasta)
-        seq_records = list(SeqIO.parse(fasta_path, 'fasta'))
-        seq = [str(record.seq) for record in seq_records if not 'plasmid' in record.description]
-        seq = ''.join(seq)
+        seq_records = list(SeqIO.parse(fasta_path, "fasta"))
+        seq = [str(record.seq) for record in seq_records if not "plasmid" in record.description]
+        seq = "".join(seq)
 
         onehot = util.seq2onehot(seq)
         onehot = onehot[:, :4]
 
-        output_onehot_fn = fasta + '.npy'
+        output_onehot_fn = fasta + ".npy"
         output_onehot_path = os.path.join(output_dir, output_onehot_fn)
-        
+
         np.save(output_onehot_path, onehot.astype(np.int8))
 
+
 def fasta_to_onehot_file(input_path, output_path):
-    seq_records = list(SeqIO.parse(input_path, 'fasta'))
-    seq = [str(record.seq) for record in seq_records if not 'plasmid' in record.description]
-    seq = ''.join(seq)
+    seq_records = list(SeqIO.parse(input_path, "fasta"))
+    seq = [str(record.seq) for record in seq_records if not "plasmid" in record.description]
+    seq = "".join(seq)
 
     onehot = util.seq2onehot(seq)
     onehot = onehot[:, :4]
 
     np.save(output_path, onehot.astype(np.int8))
+
 
 def main(args):
     if os.path.isdir(args.input):
@@ -47,8 +47,9 @@ def main(args):
     else:
         fasta_to_onehot_file(args.input, args.output)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert fasta file to onehot encoding')
-    parser.add_argument('input', help='Input fasta file or directory')
-    parser.add_argument('output', help='Output onehot encoding file or directory')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert fasta file to onehot encoding")
+    parser.add_argument("input", help="Input fasta file or directory")
+    parser.add_argument("output", help="Output onehot encoding file or directory")
     args = parser.parse_args()
