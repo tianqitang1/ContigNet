6d235c591067f75a98479e03f30df3fc3bbb4ddc
Tianqi Tang <tianqit@usc.edu>
2023-03-08 15:04:00 -0800

    Fix packaging and CI

--- tests/test_ContigNet.py
+++ tests/test_ContigNet.py
@@ -1,6 +1,12 @@
 import os
+import sys
+import ContigNet
+
+__all__ = ['ContigNet',]
 
 
 def test_ContigNet():
-    assert os.system("python -m ContigNet --host_dir demo/host_fasta --virus_dir demo/virus_fasta --output result.csv") == 0
-    # assert os.system("python -m ContigNet --host_dir demo/host_fasta --virus_dir demo/virus_fasta --output result.csv --cpu") == 0
+    host_dir = os.path.join(os.path.dirname(sys.modules["ContigNet"].__file__), "demo/host_fasta")
+    virus_dir = os.path.join(os.path.dirname(sys.modules["ContigNet"].__file__), "demo/virus_fasta")
+    assert os.system(f"python -m ContigNet --host_dir {host_dir} --virus_dir {virus_dir} --output result.csv") == 0
+    assert os.system(f"python -m ContigNet --host_dir {host_dir} --virus_dir {virus_dir} --output result.csv --cpu") == 0
